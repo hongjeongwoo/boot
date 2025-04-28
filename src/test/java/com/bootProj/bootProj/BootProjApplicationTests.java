bbdc53729704caa4d9430bf9ad29a492c4dcaf5e
proho <prohonghong@gmail.com>
2025-04-28 10:46:53 +0900

    init

---
+++ src/test/java/com/bootProj/bootProj/BootProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootProj.bootProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
